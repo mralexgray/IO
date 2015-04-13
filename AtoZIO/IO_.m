9e4596ffa14592cfb48ef87ec5a2e987a1579700
Alex Gray <alex@mrgray.com>
2015-04-13 15:12:56 -0400

    made options as arrays and varargs options

--- AtoZIO/IO_.m
+++ AtoZIO/IO_.m
@@ -9,11 +9,11 @@
 
 SYNTHESIZE_SINGLETON_FOR_CLASS(IOOpts,shared)
 
-- _IsIt_ wantsHelp { return [IO.args any:^BOOL(id o) { return [o containsString:@"help"]; }]; }
+_IT wantsHelp { return [IO.args any:^BOOL(id o) { return [o containsString:@"help"]; }]; }
 
 _VD test __List_ args { NSLog(@"inside the vageen :%@", specialArgs = args); }
 
-- _Text_ help {  return [[_rules reduce:^id(id memo, id key, id value) {
+_TT help {  return [[_rules reduce:^id(id memo, id key, id value) {
 
   _List otherKeys = value[@"keys"];
 
@@ -22,14 +22,11 @@ _VD test __List_ args { NSLog(@"inside the vageen :%@", specialArgs = args); }
              otherKeys.count ? [otherKeys joinedBy:@",  -"] : @"", value[@"brief"]];
 
   } withInitialMemo:$(@"\n%@ %@\n\n", [@"Usage:"[PINK] ioString],[_PI.arguments[0][BLUE]ioString])]
-//#ifdef DEBUG
-                        stringByAppendingFormat:@"\n\n%@",[self.getOpts.stringRep[ORANGE] ioString]];
-//#else
-//                                                                                             string];
-//#endif
-}
+                       stringByAppendingFormat:@"\n\n%@",[self.getOpts.stringRep[ORANGE] ioString]];
 
-- _Dict_ getOpts { // if ((!parseAgain || !rules.count) && _getOpts) return _getOpts;
+} //#ifdef DEBUG #else string]; #endif
+
+_DT getOpts { // if ((!parseAgain || !rules.count) && _getOpts) return _getOpts;
 
   id opts = @{}.mutableCopy; __block id flag;
 
@@ -59,37 +56,38 @@ _VD test __List_ args { NSLog(@"inside the vageen :%@", specialArgs = args); }
   return _getOpts = [opts copy];
 }
 
-_VD getOpt __Text_ usge
-      with __Text_ key __ ... { // alternates for key
+/// @[@"Uasge" [optional], @"key"[required, make CAPS if opt itself is REQUIRED!], @"k", @"keeey" [optional]
 
-  azva_list_to_nsarray(key,keys);
+_VD registerOpts __List_ arrayOfOptsArrays __ ... {
 
-  (_rules = _rules ?: @{}.mC)[keys.shift] = @{@"keys": keys, @"brief" : usge };
+  [self performSelector:@selector(__registerOpt:) withObjects:arrayOfOptsArrays];
+}
+
+_VD __registerOpt __List_ opt { _rules = _rules ?: @{}.mC;
+
+  NSParameterAssert(opt && opt.count);
+
+  id key = opt.count > 1 ? opt[1] : opt[0];
+
+  _rules[[key lowercaseString]] = @{  @"keys" : [opt subarrayFromIndex:opt.count > 1 ? 2 : 1],
+                                     @"brief" : opt.count > 1 ? opt[0] : @"",
+                                  @"required" : @([key isUppercase]) };
+}
+
+_VD getOpt __Text_ usageThenKeyThenShortOpts __ ... {
+
+  azva_list_to_nsarray(usageThenKeyThenShortOpts,opts); [self __registerOpt:opts];
 }
 
 @end
 
 
-
-
-//
-//    }) : ({
-//
 //      if(flag)  [opts[flag] addObject:arg];
-////      else   {  [opts[@"?"] = opts[@"?"] ?: @[].mC addObject:arg];
-//
-//      else {
-//
-//        flag = arg; opts[flag] = @[].mC;
-//      }
-//
+//      else   {  [opts[@"?"] = opts[@"?"] ?: @[].mC addObject:arg];
+//      else { flag = arg; opts[flag] = @[].mC; }
 //    });
-
-
 //  [_rules each:^(id primaryKey, id info) {
-//
 //    if ([d.allKeys containsAny:info[@"keys"]]) {
-//
 //      d[primaryKey] = [info[@"keys"] reduce:d[primaryKey]?:@[].mC
 //                                  withBlock:^id(id sum, id obj) { id set;
 //        if ((set = [d objectForKey:obj]))
@@ -99,30 +97,12 @@ _VD getOpt __Text_ usge
 //  }];
 //  parseAgain = NO;
 //  return _getOpts = [d copy];
-
-
-//    else {
-//
-//    }
-//      else {
-//
-//        if (argIsFlag) fl
-//       //argIsFlag && ![opts objectForKey:flag = arg] ? ({ opts[flag] = @[].mC; }) : ({
-//
+//    else {} else { if (argIsFlag) fl //argIsFlag && ![opts objectForKey:flag = arg] ? ({ opts[flag] = @[].mC; }) : ({
 ////        id existing = opts[flag]; // doesn't have - prefix ... adding or creating a value.
-//
-//
 //        else [opts[flag] addObject:arg];
-//
 //      }) : ({ [opts[@"?"] = opts[@"?"] ?: @[].mC addObject:arg];
-
 //       arrayByAddingObject:arg].mC;
 //                                  [existing isKindOfClass:NSArray.class] ?
 //                                  [existing arrayByAddingObject:arg]     : @[existing, arg]; });
-
-
-//    }) : ({ argIsFlag && !opts[flag = arg] ? ({ opts[flag] = @[].mC; })  // no current flag. save value.. create new flag capture.
-
-//       : ({ opts[@"?"] = [opts[@"?"] ?: @[] arrayByAddingObject:arg]; flag = nil; }); // No '-', add to unnamed array.
-
-//    });
+// }) : ({ argIsFlag && !opts[flag = arg] ? ({ opts[flag] = @[].mC; })  // no current flag. save value.. create new flag capture.
+// : ({ opts[@"?"] = [opts[@"?"] ?: @[] arrayByAddingObject:arg]; flag = nil; }); // No '-', add to unnamed array. });
